c650457672276c7d69ea5c046287743112ff2ef9
mat sadler <mat@redstudio.co.uk>
2010-11-29 17:22:21 +0000

    minor change to guide admin

--- system/application/views/guides/guide_admin.php
+++ system/application/views/guides/guide_admin.php
@@ -1,5 +1,19 @@
-guide admin
-
+guide admin (only visible if you have admin rights)
+<script type="text/javascript"> 
+ 
+/* Here we replace each element with class 'wymeditor'
+ * (typically textareas) by a WYMeditor instance.
+ * 
+ * We could use the 'html' option, to initialize the editor's content.
+ * If this option isn't set, the content is retrieved from
+ * the element being replaced.
+ */
+ 
+jQuery(function() {
+    jQuery('.wymeditor').wymeditor();
+});
+ 
+</script> 
 <?php  foreach($guide as $key => $admin): ?>
 
 
@@ -10,7 +24,8 @@ guide admin
 Title: <?=form_input('title', $admin['title'])?>
 <br/>
 Filename: <input type="text" name="filename" id="filename" value="<?=$admin['filename']?>"><br/>
-<textarea cols=75 rows=20 name="description" id="description" class='wymeditor'><?=$admin['description'];?></textarea>
+<textarea cols=75 rows=20 name="description" id="description" class='wymeditor'><?=$admin['description'];?></textarea><br/>
+
 
 <input type="submit" class="wymupdate" />
 <?=form_close()?> 
