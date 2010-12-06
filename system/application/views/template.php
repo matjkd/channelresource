2dadd2e9eca11540b0e9ce115444a29844bc974d
mat sadler <mat@redstudio.co.uk>
2010-12-06 10:22:21 +0000

    minor change to accordion on guides

--- system/application/views/template.php
+++ system/application/views/template.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-gb" lang="en-gb">
 
-<?php $this->load->view('global/header'); 
-
-
-?>
+<?php $this->load->view('global/header'); ?>
 
 <body id="bd" class="  fs3"> 
 <a name="Top" id="Top"></a>
