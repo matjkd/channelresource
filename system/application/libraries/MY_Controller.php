54faaab413040a83014059906ff4ae934b0dadae
Mat Sadler <mat@redstudio.co.uk>
2011-06-23 15:31:59 +0100

    changed logo and address for printout

--- system/application/libraries/MY_Controller.php
+++ system/application/libraries/MY_Controller.php
@@ -5,17 +5,17 @@ class MY_Controller extends Controller {
 	function MY_Controller () {
 	parent::Controller();
 	
-	$config_data['config_company_name'] = "Proctor Consulting";
-	$config_data['config_company_short'] = "Company";
-	$config_data['config_address'] = "Address";
+	$config_data['config_company_name'] = "Lease-Desk Ltd";
+	$config_data['config_company_short'] = "Lease-Desk";
+	$config_data['config_address'] = "";
 	$config_data['config_address1'] = "Lake View Drive";
 	$config_data['config_address2'] = "Sherwood Park";
 	$config_data['config_address3'] = "Nottingham";
 	$config_data['config_address4'] = "NG15 0DT";
-	$config_data['config_address5'] = "Address";
+	$config_data['config_address5'] = "";
 	$config_data['config_version'] = "0.0.9";
 	$config_data['config_email'] = "support@lease-desk.com";
-	$config_data['config_website'] = "www.proctorconstulting.co.uk";
+	$config_data['config_website'] = "www.lease-desk.com";
 	$config_data['config_phone'] = "01302 245310";
 	$config_data['currency'] = "&pound;";
 	
