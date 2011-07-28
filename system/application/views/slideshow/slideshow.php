95221ab9df0978eb4369ff4ef1da93e54ba54d18
Mat Sadler <mat@redstudio.co.uk>
2011-07-28 16:10:07 +0100

    added an ie7 fix

--- system/application/views/slideshow/slideshow.php
+++ system/application/views/slideshow/slideshow.php
@@ -1,7 +1,7 @@
 <div class="slide_container">
     <div class="cycle" style="display:none;">
-<img width="825px"  src="<?=base_url()?>images/slides/consultancy/slide1.jpg"/>
+<img width="825px"  height="214px" src="<?=base_url()?>images/slides/consultancy/slide1.jpg"/>
 
-<img width="825px" src="<?=base_url()?>images/slides/consultancy/slide4.jpg"/>
+<img width="825px"  height="214px" src="<?=base_url()?>images/slides/consultancy/slide4.jpg"/>
 </div>
 </div>
