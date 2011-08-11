6a8e165a8096de2d18f04fea6d8d5123bcac4417
Mat Sadler <mat@redstudio.co.uk>
2011-08-11 09:31:39 +0100

    fixed typo in s3 library

--- system/application/libraries/S3.php
+++ system/application/libraries/S3.php
@@ -1437,8 +1437,8 @@ final class S3Request {
 		curl_setopt($curl, CURLOPT_URL, $url);
 
                  //this is a fix for the bytemark server, post needs to go through proxy
-                curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
-                curl_setopt($ch, CURLOPT_PROXY, 'localhost');
+                curl_setopt($curl, CURLOPT_PROXYPORT, 3128);
+                curl_setopt($curl, CURLOPT_PROXY, 'localhost');
 
 		// Headers
 		$headers = array();
