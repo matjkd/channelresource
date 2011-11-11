60c876dad05fc0b321edfc1db512fc29eb6409e3
Mat Sadler <mat@redstudio.co.uk>
2011-11-11 16:57:20 +0000

    removed mobile tracking cos of error in blackberry

--- system/application/views/mobilelogintemplate.php
+++ system/application/views/mobilelogintemplate.php
@@ -1,29 +1,4 @@
-<?php
-  // Copyright 2010 Google Inc. All Rights Reserved.
-
-  $GA_ACCOUNT = "MO-19623681-11";
-  $GA_PIXEL = "/ga.php";
-
-  function googleAnalyticsGetImageUrl() {
-    global $GA_ACCOUNT, $GA_PIXEL;
-    $url = "";
-    $url .= $GA_PIXEL . "?";
-    $url .= "utmac=" . $GA_ACCOUNT;
-    $url .= "&utmn=" . rand(0, 0x7fffffff);
-    $referer = $_SERVER["HTTP_REFERER"];
-    $query = $_SERVER["QUERY_STRING"];
-    $path = $_SERVER["REQUEST_URI"];
-    if (empty($referer)) {
-      $referer = "-";
-    }
-    $url .= "&utmr=" . urlencode($referer);
-    if (!empty($path)) {
-      $url .= "&utmp=" . urlencode($path);
-    }
-    $url .= "&guid=ON";
-    return str_replace("&", "&amp;", $url);
-  }
-?><!doctype html>
+<!doctype html>
 <!-- Conditional comment for mobile ie7 http://blogs.msdn.com/b/iemobile/ -->
 <!--[if IEMobile 7 ]>    <html class="no-js iem7"> <![endif]-->
 <!--[if (gt IEMobile 7)|!(IEMobile)]><!--> <html class="no-js"> <!--<![endif]-->
