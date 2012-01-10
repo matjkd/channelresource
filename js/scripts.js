3e30f9b889e9ca4b29c7846654bfe048b1b58fa6
Mat Sadler <mat@redstudio.co.uk>
2012-01-10 16:47:40 +0000

    added correct id for sending emails from table

--- js/scripts.js
+++ js/scripts.js
@@ -146,7 +146,7 @@ $(function() {
     var quoteID = $('#top_controls').attr('class'),    
     email = $("#email"),
     emessage = $("#emessage"),
-     email2 = $("#emailtable"),
+    email2 = $("#emailtable"),
     emessage2 = $("#emessage2"),
     tips = $( ".validateTips" );
     
@@ -230,8 +230,8 @@ $(function() {
                 
                 
                 if ( bValid ) {
-                  
-                    $.post('/quote/results/' + tableQuoteID + '/email', {
+                 
+                    $.post('/quote/results/' + tableQuoteID2 + '/email', {
                         email: email2.val(),
                         emessage: emessage2.val()
                     }, function(data) {
@@ -245,37 +245,7 @@ $(function() {
         }
     });
     
-    $( "#edit-note" ).dialog({
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            Cancel: function() {
-                //alert(email.val());
-                $( this ).dialog( "close" );
-            },
-            Send: function() {
-               
-                var bValid = true;
-                //check email is valid
-                bValid = bValid && checkLength( email, "email", 6, 80 );
-                bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. user@example.com" );
-                
-                
-                if ( bValid ) {
-                  
-                    $.post('/support/edit_note/' + noteid , {
-                        email: email.val(),
-                        emessage: emessage.val()
-                    }, function(data) {
-                        alert(data);
-                        $("#dialog-form").dialog('close');
-                    });
-                }
-             
-                
-            }
-        }
-    });
+    
 
 
 
