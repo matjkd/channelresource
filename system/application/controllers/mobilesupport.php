0ed93cc1c9a50cb19b39ce5081577b30c07bbc8d
Mat Sadler <mat@redstudio.co.uk>
2012-01-12 15:46:41 +0000

    additions to mobile support site

--- system/application/controllers/mobilesupport.php
+++ system/application/controllers/mobilesupport.php
@@ -17,6 +17,7 @@ class Mobilesupport extends My_Controller {
         $this->load->model('membership_model');
         $this->load->plugin('to_pdf');
         $this->load->library('user_agent');
+        $this->load->library('s3');
     }
 
     function index() {
@@ -42,27 +43,97 @@ class Mobilesupport extends My_Controller {
         //get ticket data
         $data['ticket_data'] = $this->support_model->get_ticket($support_id);
         foreach ($data['ticket_data'] as $row):
-            
+            $data['support_id'] = $support_id;
             $data['user_id'] = $row['user_id'];
             $data['company_id'] = $row['company_id'];
             $data['telephone'] = $row['telephone'];
             $data['email_address'] = $row['email_address'];
             $data['support_subject'] = $row['support_subject'];
-            $data['support_type'] = $row['support_type'];
+
             $data['support_issue'] = $row['support_issue'];
             $data['support_description'] = $row['support_description'];
-            $data['support_status'] = $row['support_status'];
+
             $data['completion_date'] = $row['completion_date'];
             $data['support_priority'] = $row['support_priority'];
-            
+
+//TODO make this more elegant as it currently requires changes on multiple pages if a change is made
+            if (($row['support_type']) == 1) {
+                $type = "Lease-Desk.com";
+            }
+            if (($row['support_type']) == 2) {
+                $type = "Channel-Resource";
+            }
+            if (($row['support_type']) == 3) {
+                $type = "Customer-Resource";
+            }
+            if (($row['support_type']) == 4) {
+                $type = "Training";
+            }
+            if (($row['support_type']) == 5) {
+                $type = "Account Review";
+            }
+
+            $data['support_type'] = $type;
+
+            if (($row['support_priority']) == 1) {
+                $priority = "<span style='color:red;'>URGENT</span>";
+            }
+            if (($row['support_priority']) == 2) {
+                $priority = "High";
+            }
+            if (($row['support_priority']) == 3) {
+                $priority = "Medium";
+            }
+            if (($row['support_priority']) == 4) {
+                $priority = "Low";
+            }
+
+            $data['support_priority'] = $priority;
+
+            if (($row['support_status']) == 1) {
+                $status = "Submitted";
+            }
+            if (($row['support_status']) == "Submitted") {
+                $status = "Submitted";
+            }
+            if (($row['support_status']) == 2) {
+                $status = "Assigned";
+            }
+            if (($row['support_status']) == 3) {
+                $status = "Closed";
+            }
+
+            if (($row['support_status']) == 4) {
+                $status = "Accepted";
+            }
+
+            if (($row['support_status']) == 5) {
+                $status = "Awaiting Customer";
+            }
+
+            if (($row['support_status']) == 6) {
+                $status = "Resolved";
+            }
+
+            if (($row['support_status']) == 7) {
+                $status = "Development";
+            }
+            $data['support_status'] = $status;
         endforeach;
-        
-         //convert channel partner id into the name
+
+        //convert channel partner id into the name
         $data['channel_detail'] = $this->Membership_model->get_company_detail($data['company_id']);
         foreach ($data['channel_detail'] as $row):
             $data['channel_partner_name'] = $row['company_name'];
         endforeach;
         //end of conversion
+        //
+        //
+        //List File attachments
+        $bucketname = "lease-desk";
+        $data['mainbucket'] = "lease-desk";
+        $data['bucket_name'] = $data['ticket_id'];
+        $data['bucket_contents'] = $this->s3->getBucket($bucketname);
 
         $data['desktop'] = 'support';
         $data['main'] = '/support/mobile/view_request';
--- system/application/views/support/mobile/view_request.php
+++ system/application/views/support/mobile/view_request.php
@@ -1,7 +1,28 @@
-<div class="ui-body ui-body-b">
-<p><strong>Subject:</strong> <?=$support_subject?></p>
-<p><strong>Company:</strong> <?=$channel_partner_name?></p>
-<p><strong>Telephone:</strong> <?=$telephone?></p>
-<p><strong>Email:</strong> <?=$email_address?></p>
-<p><strong>Description:</strong> <?=$support_description?></p>
-</div>
+
+<ul data-role="listview" data-inset="true">
+
+    <li data-role="list-divider">
+        Support Request ID: <?= $support_id ?> - <?= $support_status ?>
+    </li>
+    <li>Priority:<p class="ui-li-aside"><strong> <?= $support_priority ?></strong></p></li>
+    <li>Subject:<p class="ui-li-aside"><strong> <?= $support_subject ?></strong></p></li>
+    <li>Company:<p class="ui-li-aside"><strong> <?= $channel_partner_name ?></strong></p></li>
+    <li>Telephone:<p class="ui-li-aside"><strong> <?= $telephone ?></strong></p></li>
+    <li>Email:<p class="ui-li-aside"><strong> <?= $email_address ?></strong></p></li>
+    <li>Support Type:<p class="ui-li-aside"><strong> <?= $support_type ?></strong></p></li>
+    <li>Priority:<p class="ui-li-aside"><strong> <?= $support_priority ?></strong></p></li>
+    <li data-role="list-divider">
+        Description
+    </li>
+    <li><p><?= $support_description ?></p></li>
+</ul>
+
+
+<ul data-role="listview" data-inset="true">
+
+    <li data-role="list-divider">
+       Notes
+    </li>
+    <li>Note:<p class="ui-li-aside">added by blah</p><br/>text</li>
+  
+</ul>
