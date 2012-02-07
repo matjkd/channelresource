d132e347ec413abcb7ea2ef22a1c93eddf867940
Mat Sadler <mat@redstudio.co.uk>
2012-02-07 11:03:57 +0000

    [#23338389] updated to latest version of jquery mobile (1.0.1)

--- system/application/views/mobilelogintemplate.php
+++ system/application/views/mobilelogintemplate.php
@@ -26,7 +26,7 @@
         <link rel="apple-touch-icon-precomposed" href="images/mobile/l/apple-touch-icon-precomposed.png">
 
    
-         <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.css" />
 
         <!-- For nokia devices: -->
         <link rel="shortcut icon" href="images/mobile/l/apple-touch-icon.png">
@@ -40,9 +40,9 @@
         <link rel="stylesheet" href="<?= base_url() ?>css/themes/mobile/mobile.css?v=1">
 
 
-        <!-- scripts concatenated and minified via ant build script -->
+       <!-- scripts concatenated and minified via ant build script -->
         <script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
- <script type="text/javascript" src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
+        <script src="http://code.jquery.com/mobile/1.0.1/jquery.mobile-1.0.1.min.js"></script>
 
 
 
