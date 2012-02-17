79ba3845abd586eee4b7f28aa4beb11e21904d34
Mat Sadler <mat@redstudio.co.uk>
2012-02-17 15:15:06 +0000

    [#23347297] priority and status bits updated on mobile site

--- system/application/views/support/mobile/list.php
+++ system/application/views/support/mobile/list.php
@@ -2,100 +2,71 @@
     <ul data-role="listview" id="main_list" data-split-icon="gear" data-split-theme="d" data-filter="true">
 
         <?php
-        if(isset($ticket_list)) {
-        foreach ($ticket_list as $key => $row):
+        $priority = "";
+        $type = "";
 
-            if (($row['support_type']) == 1) {
-                $type = "Lease-Desk.com";
-            }
-            if (($row['support_type']) == 2) {
-                $type = "Channel-Resource";
-            }
-            if (($row['support_type']) == 3) {
-                $type = "Customer-Resource";
-            }
-            if (($row['support_type']) == 4) {
-                $type = "Training";
-            }
-            if (($row['support_type']) == 5) {
-                $type = "Account Review";
-            }
+        $order = "";
+        if (isset($ticket_list)) {
+            foreach ($ticket_list as $key => $row):
+
+                foreach ($areas as $statusrow):
+
+                    if (($row['support_type']) == $statusrow['status_value']) {
+                        $type = $statusrow['status_name'];
+                    }
+                endforeach;
 
 
+                foreach ($prioritylist as $statusrow):
 
-            if (($row['support_priority']) == 1) {
-                $priority = "1.<span style='color:red;'>URGENT</span>";
-            }
-            if (($row['support_priority']) == 2) {
-                $priority = "2.High";
-            }
-            if (($row['support_priority']) == 3) {
-                $priority = "3.Medium";
-            }
-            if (($row['support_priority']) == 4) {
-                $priority = "4.Low";
-            }
-            if (($row['support_priority']) == 5) {
-                $priority = "5.Closed";
-            }
+                    if (($row['support_priority']) == $statusrow['status_value']) {
+                        $priority = $statusrow['status_name'];
+                        $order = $statusrow['status_value'];
+                    }
+                    if ($statusrow['status_value'] == 1) {
+                        $priority = "<span  style='color:red;'>" . $priority . "</span>";
+                    } else if ($statusrow['status_value'] != 1) {
+                        $priority = "<span  style='color:#000000;'>" . $priority . "</span>";
+                    }
+                endforeach;
 
-            //   $statusarray = array(1 => 'Submitted', 4 => 'Accepted', 2 => 'Assigned', 5 => 'Awaiting Customer', 6=> 'Resolved', 7=> 'Development', 3 => 'CLOSED');
 
-            if (($row['support_status']) == 1) {
-                $status = "Submitted";
-            }
-            if (($row['support_status']) == "Submitted") {
-                $status = "Submitted";
-            }
-            if (($row['support_status']) == 2) {
-                $status = "Assigned";
-            }
-            if (($row['support_status']) == 3) {
-                $status = "Closed";
-            }
+                foreach ($statuslist as $statusrow):
 
-            if (($row['support_status']) == 4) {
-                $status = "Accepted";
-            }
+                    if (($row['support_status']) == $statusrow['status_value']) {
+                        $status = $statusrow['status_name'];
+                    }
+                endforeach;
+                if (!isset($status)) {
+                    $status = "Submitted.";
+                }
 
-            if (($row['support_status']) == 5) {
-                $status = "Awaiting Customer";
-            }
-
-            if (($row['support_status']) == 6) {
-                $status = "Resolved";
-            }
-
-            if (($row['support_status']) == 7) {
-                $status = "Development";
-            }
-
-            $old_date_added = strtotime($row['date_added']);
-            $new_date_added = date('l jS \of F Y h:i:s A', $old_date_added);
-            ?>
+                $old_date_added = strtotime($row['date_added']);
+                $new_date_added = date('l jS \of F Y h:i:s A', $old_date_added);
+                ?>
 
 
 
-            <li><a href="<?= base_url() ?>mobilesupport/view_support_request/<?= $row['support_id'] ?>" data-ajax="false">
-                    <p><?= $priority ?></p>
-                    <p><strong>Ref:</strong> <?= $row['support_id'] ?></p>
+                <li><a href="<?= base_url() ?>mobilesupport/view_support_request/<?= $row['support_id'] ?>" data-ajax="false">
+                        <p><?= $priority ?></p>
+                        <p><strong>Ref:</strong> <?= $row['support_id'] ?></p>
 
 
-                    <p><strong>Subject:</strong> <?= $row['support_subject'] ?></p>
-                    <p><strong>Company:</strong> <?= $row['company_name'] ?></p>
-                    <p><strong>Type:</strong> <?= $type ?></p>
-                    <p><strong>Status:</strong> <?= $status ?></p>
+                        <p><strong>Subject:</strong> <?= $row['support_subject'] ?></p>
+                        <p><strong>Company:</strong> <?= $row['company_name'] ?></p>
+                        <p><strong>Type:</strong> <?= $type ?></p>
+                        <p><strong>Status:</strong> <?= $status ?></p>
 
-                    <p><?= $new_date_added ?></p>
-                </a>
-            </li>
+                        <p><?= $new_date_added ?></p>
+                    </a>
+                </li>
 
 
 
-        <?php endforeach; ?>
-            
-            <?php } else {?>
-No Requests to display
-<?php } ?>
+            <?php endforeach; ?>
+
+        <?php } else { ?>
+            No Requests to display
+        <?php } ?>
     </ul>
 </div>
