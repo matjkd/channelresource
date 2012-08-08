d13a80a6f024c59cc96d810c503261d35a445c6f
Mat Sadler <mat@redstudio.co.uk>
2012-08-08 14:42:13 +0100

    changed some email links

--- system/application/views/main/main_customer.php
+++ system/application/views/main/main_customer.php
@@ -1,6 +1,6 @@
 <div style="width:100%; float:left">
 <p>Our dedicated Customer portal providing you with essential support tools and resources,
-enabling you to confidently use Lease-Desk.com and stay up to date on future product developments.</p>
+enabling you to confidently use <strong>cc</strong>apps. and stay up to date on future product developments.</p>
 <p>On the right hand tool bar you will find a list of options available to you.
  If you have any questions or require any assistance, please contact us:</p>
 </div>
@@ -11,7 +11,7 @@ enabling you to confidently use Lease-Desk.com and stay up to date on future pro
 
 
  <div class="videoholder" style="height:143px; padding:0px; width:198px; float:left; background:#8fa1a7; margin:3px 5px 5px 0px;">
-                <a href="mailto:support@lease-desk.com"  title=""><img alt="Contact Us" src="<?=base_url()?>images/icons/contactbox.jpg"/></a>
+                <a href="mailto:support@ccapps.cc"  title=""><img alt="Contact Us" src="<?=base_url()?>images/icons/contactbox.jpg"/></a>
     </div>
         <div class="video1" style="height:143px; padding:0px; width:199px; float:left; background:#dddddd; margin:3px 5px 5px 5px;">
               <a href="http://www.youtube.com/watch?v=9xHxoSLhtn8" rel="prettyPhoto" title=""><img alt="Welcome" src="<?=base_url()?>images/icons/welcome.jpg"/></a>
